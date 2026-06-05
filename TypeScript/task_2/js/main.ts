fc76ad99d2990075f407ec7570320be6fa656847
Muhammed Ehmedov <ahmedov.muhammed.16112006@gmail.com>
2026-06-05 22:46:55 +0400

    Update main.ts

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -1,3 +1,53 @@
+    interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [key: string]: any;
+    }
+
+    interface Directors extends Teacher {
+    numberOfReports: number;
+    }
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+console.log(director1);
+
+class StudentClass implements StudentClassInterface {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return 'Currently working';
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
+
+interface StudentClassInterface {
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+interface StudentClassConstructor {
+    new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+
 interface DirectorInterface {
     workFromHome(): string;
     getCoffeeBreak(): string;
