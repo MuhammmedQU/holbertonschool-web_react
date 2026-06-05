1beefbb6641c16ee82352eb9a8951b8e296f5be3
Muhammed Ehmedov <ahmedov.muhammed.16112006@gmail.com>
2026-06-05 21:47:58 +0400

    Update main.ts

--- TypeScript/task_1/main.ts
+++ TypeScript/task_1/main.ts
@@ -1,18 +1,21 @@
-interface Teacher {
-   readonly firstName: string;
-   readonly lastName: string;
-   fullTimeEmployee: boolean;
-   yearsOfExperience?: number;
-   location: string;
-   [key: string]: any;
-}
+    interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [key: string]: any;
+    }
 
-const teacher3: Teacher = {
-   firstName: "John",
-   lastName: "Doe",
-   fullTimeEmployee: true,
-   location: "New York",
-   contract: false
+    interface Directors extends Teacher {
+    numberOfReports: number;
+    }
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
 };
-
-console.log(teacher3);
+console.log(director1);
